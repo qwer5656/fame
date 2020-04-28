8cf466e0fd21fe20157c74a93afdeba19a770b29
qwer5656 <determine2014@gmail.com>
2020-04-28 16:25:27 +0800

    t
    t

--- zoom/script/zoom.js
+++ zoom/script/zoom.js
@@ -5,26 +5,46 @@
 
 
 }(this,function(){
-    return function(option={}){        
-        let Data={         
+    return function(container,option={}){        
+        let Data={
+            container:{
+                element:null,
+                width:0,
+                height:0
+            },
+            sourceimg:{
+                element:null,
+                width:0,
+                height:0
+            },
+            zoomlen:{
+                element:null,
+                size:0,
+            },
+            zoomdiv:{
+                element:null,
+                size:0
+            }
         }
-        Data.size=option.size?option.size:100;
-        Data.zoomsize=option.zoomsize?option.zoomsize:200;
-        let w=Data.size/2;
+        Data.container.element=container;
+        Data.zoomdiv.size=option.size?option.size:100;
+        Data.zoomlen.zoomsize=option.zoomsize?option.zoomsize:200;
+        let w=Data.zoomdiv.size/2;
         let div=document.createElement("div");
-        let content=document.querySelector(".content");  
-        let zoom=document.querySelector(".zoom");  
-        zoom.style.width=`${Data.zoomsize}px`;
-        zoom.style.height=`${Data.zoomsize}px`;
+         Data.zoomlen.element=document.querySelector(".zoom");  
         let img=document.querySelector(".sourceimg");
-        let data=content.getBoundingClientRect();
-        let zoominfo=zoom.getBoundingClientRect();
-        let sizeX=zoominfo.width/Data.size;
-        let sizeY=zoominfo.height/Data.size;
-        let zoomlens=content.appendChild(div);
-        zoom.style.backgroundSize=`${data.width*sizeX}px ${data.height*sizeY}px`;
-        zoomlens.style.width=`${Data.size}px`;
-        zoomlens.style.height=`${Data.size}px`;
+        let data=Data.container.element.getBoundingClientRect();
+
+        Data.zoomlen.element.style.width=`${Data.zoomlen.zoomsize}px`;
+        Data.zoomlen.element.style.height=`${Data.zoomlen.zoomsize}px`;
+
+        let zoominfo= Data.zoomlen.element.getBoundingClientRect();
+        let sizeX=zoominfo.width/Data.zoomdiv.size
+        let sizeY=zoominfo.height/Data.zoomdiv.size;
+        let zoomlens=Data.container.element.appendChild(div);
+        Data.zoomlen.element.style.backgroundSize=`${data.width*sizeX}px ${data.height*sizeY}px`;
+        zoomlens.style.width=`${Data.zoomdiv.size}px`;
+        zoomlens.style.height=`${Data.zoomdiv.size}px`;
         zoomlens.style.backgroundColor="#cecece";
         zoomlens.style.position="absolute";  
         zoomlens.style.opacity=".2";
@@ -33,9 +53,9 @@
         let clientx=data.left;
         let clienty=data.top;
         let display=false;
-        content.addEventListener("mousemove",event);
-        content.addEventListener("mouseenter",event)
-        content.addEventListener("mouseleave",event);
+        Data.container.element.addEventListener("mousemove",event);
+        Data.container.element.addEventListener("mouseenter",event)
+        Data.container.element.addEventListener("mouseleave",event);
         function event(e)
         {
             switch(e.type)
@@ -54,10 +74,10 @@
 
             function mouseleave(){
                 zoomlens.style.display="none";
-                zoom.style.display="none";
+                 Data.zoomlen.element.style.display="none";
             }
             function mouseenter(){
-                zoom.style.display="block";
+                 Data.zoomlen.element.style.display="block";
                 zoomlens.style.display="block";
             }
             function mousemove(){
@@ -65,7 +85,7 @@
                 zoomlens.style.left=getpos(e.clientX-clientx-w,data.width)+"px";
                 let x=-getpos(e.clientX-clientx-w,data.width)*sizeX+"px"
                 let y=-getpos(e.clientY-clienty-w,data.height)*sizeY+"px"
-                zoom.style.backgroundPosition =`${x} ${y}`;
+                Data.zoomlen.element.style.backgroundPosition =`${x} ${y}`;
             }
         }
         function getpos(e,size){
